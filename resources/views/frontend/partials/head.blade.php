f1586dc2e9e4f69b636d55e83a59aede7db8239a
ashiquzzaman77 <ashiquzzaman.rajib.cse@!gmail.com>
2024-10-09 15:35:54 +0600

    Update By Ashiquzzaman 07-10-2024

--- resources/views/frontend/partials/head.blade.php
+++ resources/views/frontend/partials/head.blade.php
@@ -33,17 +33,17 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="stylesheet" href="{{ asset('frontend/assets/Css/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/assets/Css/boxicons.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/assets/Css/meanmenu.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/assets/Css/magnific-popup.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/assets/Css/owl.carousel.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/assets/Css/owl.theme.default.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/assets/Css/odometer.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/assets/Css/animate.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/assets/Css/style.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/assets/Css/dark.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/assets/Css/responsive.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/Css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/Css/boxicons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/Css/meanmenu.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/Css/magnific-popup.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/Css/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/Css/owl.theme.default.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/Css/odometer.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/Css/animate.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/Css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/Css/dark.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/Css/responsive.css') }}">
 
 <link rel="icon" type="image/png" href="{{ !empty(optional($setting)->site_favicon) && file_exists(public_path('storage/' . optional($setting)->site_favicon)) ? asset('storage/' . optional($setting)->site_favicon) : asset('frontend/images/no-logo(217-55).jpg') }}">
 
