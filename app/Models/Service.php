88b34d072c832360cb8914aa02393257d11fc78e
ashiquzzaman77 <ashiquzzaman.rajib.cse@!gmail.com>
2024-10-28 11:27:14 +0600

    Update By Ashiquzzaman 29-10-2024

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -3,19 +3,18 @@
 namespace App\Models;
 
 use App\Traits\HasSlug;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Service extends Model
 {
     use HasFactory, HasSlug;
-    protected $slugSourceColumn = 'name';
+    protected $slugSourceColumn='name';
+
     /**
      * The attributes that aren't mass assignable.
      *
      * @var array
      */
     protected $guarded = [];
-
-    
 }
