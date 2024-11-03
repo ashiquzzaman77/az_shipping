4912a1ef3e7e6954a6aeae0e6062e47ccd2f3501
ashiquzzaman77 <ashiquzzaman.rajib.cse@!gmail.com>
2024-11-03 17:07:33 +0600

    Update By Ashiquzzaman 02-11-2024

--- resources/views/frontend/partials/script.blade.php
+++ resources/views/frontend/partials/script.blade.php
@@ -2,7 +2,7 @@
 
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
-<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+{{-- <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script> --}}
 
 <script src="{{ asset('frontend/js/jquery.min.js') }}"></script>
 <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
