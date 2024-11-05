c26824adbaae40f01d6ff7e9813289455c28a55c
ashiquzzaman77 <ashiquzzaman.rajib.cse@!gmail.com>
2024-11-05 12:37:25 +0600

    Update By Ashiquzzaman 05-11-2024

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -120,7 +120,7 @@
 
 <div class="footer-bottom text-center" id="contact">
     <div class="container">
-        <p>Copyright @2024. All Rights Reserved <a href="javascript:;" target="_blank"><span class="text-white">AZ Shipping</span></a>
+        <p class="mb-0">Copyright @2024. All Rights Reserved <a href="javascript:;" target="_blank"><span class="text-white">AZ Shipping</span></a>
         </p>
         <p>Development By <a href="https://www.ashikuzzaman.net/">Ashiquzzaman</a></p>
     </div>
