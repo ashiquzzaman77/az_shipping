36257ab08c0cc793e49aa1770d3bc2b3b01b5181
ashiquzzaman77 <ashiquzzaman.rajib.cse@gmail.com>
2024-11-15 23:04:22 +0600

    Update By Ashiquzzaman 10-11-2024

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -96,7 +96,9 @@
 
                         <li>
                             <i class="bx bxs-location-plus"></i>
-                            <a data-bs-toggle="modal" data-bs-target="#exampleModal" >{{ optional($setting)->address_line_one }}</a>
+                            <p data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                style="cursor: pointer;">{{ optional($setting)->address_line_one }}
+                            </p>
                         </li>
 
                         <li>
@@ -131,18 +133,18 @@
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
         </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
     </div>
-  </div>
+</div>
